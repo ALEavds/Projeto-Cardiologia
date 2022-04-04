57645166f2ccc4bb659a0c37791ccda64a7ba287
Alexandre Vieira <101663403+ALEavds@users.noreply.github.com>
2022-04-04 11:26:46 -0300

    Add files via upload

---
+++ PROJETO CARDIOLOGIA/pa/Relação de exames.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	int exame, sexo, continuar;
+	
+	do{
+	
+	printf(" Selecione um dos exames a baixo digitando seu respectivo número! \n 1.Ecocardiograma \n 2.Eletrocardiograma \n 3.Mapa \n 4.Holter \n ");
+	scanf("%d", &exame);
+	system(cls);
+	printf(" Indique o sexo do paciente com M ou F (Respectivamente Masculino ou Feminino)");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}while(continuar==1);
+	
+	return 0;
+}
